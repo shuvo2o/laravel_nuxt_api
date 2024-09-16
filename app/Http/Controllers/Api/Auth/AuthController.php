840ceee6dd37735d6347112cc0f8c70333c8a05c
shuvo2o <eng.shuvo03@gmail.com>
2024-09-16 23:20:34 +0600

    Sanctum Authentication done

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Resources\Auth\AuthResource;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request){
+
+        $user = User::where('email', $request->email)->first();
+     
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+        
+     
+        $token = $user->createToken('nuxtApiToken')->plainTextToken;
+
+
+        return AuthResource::make([
+            'token' => $token,
+            'user' => [
+                'name'=> $user->name,
+                'email'=> $user->email,
+            ],
+        ]);
+    }
+
+
+    public function register(){
+
+    }
+}
